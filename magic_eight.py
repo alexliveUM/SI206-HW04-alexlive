8e2a3fb08c4695f6807cf8da8a8f05db7fc3c56c
Alex Li <alexlive@umich.edu>
2018-01-31 16:47:46 -0500

    Added while loop

--- magic_eight.py
+++ magic_eight.py
@@ -1,2 +1,12 @@
-response = input('What is your question? ')
+while True:
+	response = input('What is your question? ')
+	
+	if response.strip() == 'quit':
+		exit(1)
+	if response.strip()[-1] != '?':
+		print('Sorry, I can only answer questions.')
+		continue
+
+
+
 
